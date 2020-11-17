c7b82a4712260cb85c4bf6035bb74de322687e90
Christoffer Lindow <cl223ru@student.lnu.se>
2020-11-17 19:08:04 +0100

    Setup

--- LiftingLibrary.api/Controllers/Home.cs
+++ LiftingLibrary.api/Controllers/Home.cs
@@ -1,7 +1,15 @@
-﻿namespace LiftingLibrary.api.Controllers
-{
-    public class Home
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LiftingLibrary.api.Controllers
+{   
+    [ApiController]
+    [Route("api/home")]
+    public class Home : ControllerBase
     {
-        
+        [HttpGet]
+        public string Index()
+        {
+            return "Hello world";
+        }
     }
 }
---
+++ LiftingLibrary.api/Startup.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace LiftingLibrary.api
+{
+    public class Startup
+    {
+        private const string ALL_CORS = "all";
+       
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+            services.AddCors(options => options.AddPolicy(ALL_CORS, build => build
+                .AllowAnyHeader().AllowAnyOrigin()));
+        }
+        
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseCors(ALL_CORS);
+
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapDefaultControllerRoute();
+            });
+        }
+    }
+}
